079d8e74bb0b8a000a57660ba2c1d48e1f393480
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2012-05-26 16:36:35 -0700

    Add a Accept request header parser.

--- request.rs
+++ request.rs
@@ -1,4 +1,7 @@
+import std::sort;
+
 export method;
+export accept;
 export request;
 
 enum method {
@@ -13,11 +16,18 @@ enum method {
     PATCH
 }
 
+type accept = {
+    mime_type: (str, str),
+    quality: float,
+    options: [str],
+};
+
 type request<T> = {
     req: @mongrel2::request,
     method: method,
     cookies: hashmap<str, cookie::cookie>,
     mut data: T,
+    mut _accepts: option<[accept]>,
 };
 
 fn request<T: copy>(req: @mongrel2::request, rep: @response, data: T)
@@ -64,7 +74,47 @@ fn request<T: copy>(req: @mongrel2::request, rep: @response, data: T)
         method: method,
         cookies: cookies,
         mut data: data,
+        mut _accepts: none,
     })
+}
+
+#[doc = "Split a mime string into the mime type and subtype."]
+fn parse_mime_type(mime_type: str) -> (str, str) {
+    let parts = str::splitn_char(mime_type, '/', 1u);
+    let typ = parts[0];
+    let subtyp = if parts.len() == 2u { parts[1] } else { "*" };
+
+    (typ, subtyp)
+}
+
+#[doc = ""]
+fn parse_accept_header(header: str) -> [accept] {
+    let accepts = header.split_char(',').map { |e|
+        let parts = str::replace(e, " ", "").split_char(';');
+
+        let mime_type = parse_mime_type(parts[0]);
+        let mut quality = 1.0;
+        let mut options = [];
+
+        if parts.len() > 1u {
+            vec::iter_between(parts, 1u, parts.len()) { |option|
+                if option.starts_with("q=") {
+                    let q = option.substr(2u, option.len() - 2u);
+                    alt float::from_str(q) {
+                      none { /* Ignore invalid quality values. */ }
+                      some(q) { quality = q; }
+                    }
+                } else {
+                    vec::push(options, option);
+                }
+            }
+        }
+
+        { mime_type: mime_type, quality: quality, options: options }
+    };
+
+    // Sort by quality, with highest quality first.
+    sort::merge_sort({ |e1, e2| e1.quality >= e2.quality }, accepts)
 }
 
 impl request<T: copy> for @request<T> {
@@ -93,5 +143,145 @@ impl request<T: copy> for @request<T> {
                 some(values[0])
             }
         }
+    }
+
+    fn content_type() -> option<str> {
+        self.find_header("content-type")
+    }
+
+    fn accepts() -> [accept] {
+        // Lazily parse the accept header.
+        alt self._accepts {
+          none {
+            let accepts = alt self.find_header("accept") {
+              none {
+                // If we don't have the header, assume we accept everything.
+                [{ mime_type: ("*", "*"), quality: 1.0, options: [] }]
+              }
+              some(accept) { parse_accept_header(accept) }
+            };
+
+            self._accepts = some(accepts);
+
+            accepts
+          }
+          some(accepts) { accepts }
+        }
+    }
+
+    fn accept(mime_type: str) -> bool {
+        let mime_type = parse_mime_type(mime_type);
+
+        for self.accepts().each { |accept|
+            if accept.mime_type == ("*", "*") { ret true; }
+            if accept.mime_type == mime_type { ret true; }
+        }
+
+        false
+    }
+
+    fn negotiate_media_types<U: copy>(mime_types: [(str, U)]) -> option<U> {
+        let mime_types = mime_types.map { |t_v|
+            let (t, v) = t_v;
+            let (t, s) = parse_mime_type(t);
+            (t, s, v)
+        };
+
+        // Walk over the accepts in order and return the first item that
+        // matches.
+        for self.accepts().each { |accept|
+            let (typ, subtyp) = accept.mime_type;
+
+            for mime_types.each { |t_s_v|
+                let (t, s, v) = t_s_v;
+
+                #error("%? %? -> %? %?", typ, subtyp, t, s);
+
+                if typ == "*" || typ == t {
+                    if subtyp == "*" || subtyp == s {
+                        ret some(v)
+                    }
+                }
+            }
+        }
+
+        none
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_parse_accept_header_firefox() {
+        let firefox = "text/html," +
+                      "application/xhtml+xml," +
+                      "application/xml;q=0.9," +
+                      "*/*;q=0.8";
+
+        assert parse_accept_header(firefox) == [
+            {
+                mime_type: ("text", "html"),
+                quality: 1.0,
+                options: []
+            },
+            {
+                mime_type: ("application", "xhtml+xml"),
+                quality: 1.0,
+                options: []
+            },
+            {
+                mime_type: ("application", "xml"),
+                quality: 0.9,
+                options: []
+            },
+            {
+                mime_type: ("*", "*"),
+                quality: 0.8,
+                options: []
+            }
+        ];
+    }
+
+    #[test]
+    fn test_parse_accept_header_webkit() {
+        let webkit = "application/xml," +
+                     "application/xhtml+xml," +
+                     "text/html;q=0.9," +
+                     "text/plain;q=0.8," +
+                     "image/png," +
+                     "*/*;q=0.5";
+
+        assert parse_accept_header(webkit) == [
+            {
+                mime_type: ("application", "xml"),
+                quality: 1.0,
+                options: []
+            },
+            {
+                mime_type: ("application", "xhtml+xml"),
+                quality: 1.0,
+                options: []
+            },
+            {
+                mime_type: ("image", "png"),
+                quality: 1.0,
+                options: []
+            },
+            {
+                mime_type: ("text", "html"),
+                quality: 0.9,
+                options: []
+            },
+            {
+                mime_type: ("text", "plain"),
+                quality: 0.8,
+                options: []
+            },
+            {
+                mime_type: ("*", "*"),
+                quality: 0.5,
+                options: []
+            }
+        ];
     }
 }
