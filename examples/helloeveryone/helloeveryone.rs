6c55cfce28574fd88c0872230c190b7a7f722fdb
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2012-10-14 15:39:35 -0700

    Update helloeveryone to work with rust 0.4.

--- examples/helloeveryone/helloeveryone.rs
+++ examples/helloeveryone/helloeveryone.rs
@@ -1,28 +1,30 @@
-import person::person;
+#[allow(non_implicitly_copyable_typarams)];
+
+use person::Person;
 
 fn main() {
     // Create a zeromq context that MRE will use to talk to Mongrel2 and
     // Elasticsearch.
-    let zmq = alt zmq::init(1) {
-        ok(ctx) { ctx }
-        err(e) { fail e.to_str() }
+    let zmq = match zmq::init(1) {
+        Ok(ctx) => ctx,
+        Err(e) => fail e.to_str(),
     };
 
-    let mre = mre::mre(
+    let mre = mre::MRE(
         zmq,
 
         // Generate a new UUID each time we start.
-        none,
+        None,
 
         // The addresses to receive requests from.
-        ~["tcp://127.0.0.1:9994"],
+        ~[~"tcp://127.0.0.1:9994"],
 
         // The addresses to send responses to.
-        ~["tcp://127.0.0.1:9995"],
+        ~[~"tcp://127.0.0.1:9995"],
 
         // Create our middleware, which preproceses requests and
         // responses. For now we'll just use the logger.
-        ~[mre::middleware::logger(io::stdout())],
+        //~[mre::middleware::logger(io::stdout())],
 
         // A function to create per-request data. This can be used by
         // middleware like middleware::session to automatically look
@@ -35,46 +37,54 @@ fn main() {
     let es = elasticsearch::connect_with_zmq(zmq, "tcp://localhost:9700");
 
     // Show who we'll say hello to.
-    do mre.get("^/$") |_req, rep, _m| {
+    do mre.get(~"^/$") |_req, rep, _m| {
         // Fetch the people we've greeted.
         let people = person::last_50(es);
 
         // We want to render out our responses using mustache, so we need
         // to convert our model over to something mustache can handle.
-        let template = mustache::render_file("index", hash_from_strs(~[
-            ("names", do people.map |person| {
-                hash_from_strs(~[
-                    ("name", person.name())
-                ])
-            }.to_mustache())
-        ]));
+        let template = mustache::render_file(
+            "index",
+            hash_from_vec(~[
+                (
+                    @~"names",
+                    mustache::Vec(
+                        @do people.map |person| {
+                            mustache::Map(
+                                hash_from_vec(~[
+                                    (@~"name", mustache::Str(@person.name()))
+                                ])
+                            )
+                        }
+                    )
+                )
+            ])
+        );
                
         rep.reply_html(200u, template)
     }
 
     // Add a new person to greet.
-    do mre.post("^/$") |req, rep, _m| {
+    do mre.post(~"^/$") |req, rep, _m| {
         // Parse the form data.
-        let form = uri::decode_form_urlencoded(*req.body());
+        let form = url::decode_form_urlencoded(copy *req.body());
 
-        alt form.find("name") {
-          none {
-            rep.reply_http(400u, "missing name");
-          }
-          some(names) {
-            // Create and save our person. If successful, redirect back to
-            // the front page.
-            let person = person::person(es, (*names)[0u]);
+        match form.find(~"name") {
+            None => rep.reply_http(400u, "missing name"),
+            Some(names) => {
+                // Create and save our person. If successful, redirect back to
+                // the front page.
+                let person = person::Person(es, copy *names[0u]);
 
-            alt person.create() {
-              ok(()) { rep.reply_redirect("/") }
-              err(e) {
-                // Uh oh, something bad happened. Let's just display the
-                // error back to the user for now.
-                rep.reply_http(500u, e.msg)
-              }
+                match person.create() {
+                    Ok(_) => rep.reply_redirect(~"/"),
+                    Err(e) => {
+                        // Uh oh, something bad happened. Let's just display the
+                        // error back to the user for now.
+                        rep.reply_http(500u, copy e.msg)
+                    }
+                }
             }
-          }
         }
     }
 
--- examples/helloeveryone/person.rs
+++ examples/helloeveryone/person.rs
@@ -1,91 +1,88 @@
-import std::time;
-import time::tm;
-import mre::model::{model, error};
+use std::time;
+use time::Tm;
+use mre::model::{Model, Error};
 
 // Create a class to act as our model. Unfortunately Rust's classes aren't
 // finished yet, and are missing a couple features that would help clean up
 // models. First, there's no way to mix in implementation of functions, so we
 // need to duplicate some code that's common across all models. Second, there
 // is no way to have multiple constructors or static functions, so we need to
-// move error handling out into a wrapper function. So to keep the api clean,
+// move Error handling out into a wrapper function. So to keep the api clean,
 // we cheat and hide the class so we can make a function that acts like what we
 // want.
-class _person {
-    let model: model;
+pub struct Person {
+    model: Model,
+}
 
-    new(model: model) {
-        self.model = model;
+pub impl Person {
+    fn id(&self) -> &self/~str {
+        &self.model._id
     }
 
-    fn id() -> @str {
-        self.model._id
+    fn timestamp(&self) -> ~str {
+        self.model.get_str(&~"timestamp")
     }
 
-    fn timestamp() -> @str {
-        self.model.get_str("timestamp")
+    fn set_timestamp(&mut self, timestamp: ~str) -> bool {
+        self.model.set_str(~"timestamp", timestamp)
     }
 
-    fn set_timestamp(timestamp: @str) -> bool {
-        self.model.set_str("timestamp", timestamp)
+    fn name(&self) -> ~str {
+        self.model.get_str(&~"name")
     }
 
-    fn name() -> @str {
-        self.model.get_str("name")
+    fn set_name(&mut self, name: ~str) -> bool {
+        self.model.set_str(~"name", name)
     }
 
-    fn set_name(name: @str) -> bool {
-        self.model.set_str("name", name)
-    }
-
-    fn create() -> result<(), error> {
+    fn create(&self) -> Result<(~str, uint), Error> {
         self.model.create()
     }
 
-    fn save() -> result<(), error> {
+    fn save(&self) -> Result<(~str, uint), Error> {
         self.model.save()
     }
 
-    fn delete() {
+    fn delete(&self) {
         self.model.delete()
     }
 }
 
-// We want the type named "person", but we don't "person" to be the built-in
-// class constructor.
-type person = _person;
-
 // Create a new person model.
-fn person(es: client, name: @str) -> person {
+pub fn Person(es: elasticsearch::Client, name: ~str) -> Person {
     // Create a person. We'll store the model in the ES index named
     // "helloeveryone", under the type "person". We'd like ES to make the index
     // for us, so we leave the id blank.
-    let person = _person(model(es, @"helloeveryone", @"person", @""));
+    let mut person = Person {
+        model: Model(es, ~"helloeveryone", ~"person", ~"")
+    };
 
     person.set_name(name);
-    person.set_timestamp(@time::now().rfc3339());
+    person.set_timestamp(time::now().rfc3339());
 
     person
 }
 
 // Return the last 50 people we have said hello to.
-fn last_50(es: client) -> ~[person] {
+pub fn last_50(es: elasticsearch::Client) -> ~[Person] {
     // This query can be a little complicated for those who have never used
     // elasticsearch. All it says is that we want to fetch 50 documents on the
     // index "helloeveryone" and the type "person", sorted by time.
     do mre::model::search(es) |bld| {
         bld
-            .set_indices(~["helloeveryone"])
-            .set_types(~["person"])
-            .set_source(*json_dict_builder()
-                .insert("size", 50.0)
-                .insert_list("sort", |bld| {
-                    bld.push_dict(|bld| {
-                        bld.insert("timestamp", "desc");
+            .set_indices(~[~"helloeveryone"])
+            .set_types(~[~"person"])
+            .set_source(JsonObjectBuilder()
+                .insert(~"size", 50.0)
+                .insert_list(~"sort", |bld| {
+                    bld.push_object(|bld| {
+                        bld.insert(~"timestamp", ~"desc");
                     });
                 })
+                .object.take()
             );
     }.map(|model|
         // Construct a person model from the raw model data.
-        _person(model)
+        Person { model: *model }
     )
 }
